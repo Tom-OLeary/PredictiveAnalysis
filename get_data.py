a4a706d8fc63c199f2778215e27544c5834d203b
Tom OLeary <troleary19@gmail.com>
2020-10-06 17:32:19 -0400

    Clean up queries

--- get_data.py
+++ get_data.py
@@ -8,45 +8,45 @@ import pandas as pd
 
 # Query to Pull Pitching Data
 pitching_query = '''
-            select sum(pitching.ER) as "earned_runs",
-                sum(pitching.IPouts) / 3 as "innings_pitched",
-                sum(pitching.H) as "H_P",
-                sum(pitching.SO) as "SO_P",
-                sum(pitching.ERA) as "ERA",
-                sum(pitching.BFP) as "batters_faced",
-                sum(pitching.BAOpp) as "Opp_BA",
-                sum(pitching.BB) as "BB_P",
-                sum(pitching.HR) as "HR_P",
-                sum(pitching.HBP) as "HBP_P",
-                sum(pitching.G) as "G_P",
-                master.throws as "throwing_arm",
-                master.retroID as "pit_id",
-                pitching.yearID as "year"
-            from pitching inner join master on pitching.playerID = master.playerID
-            where pitching.yearID >= '2012' and (pitching.IPouts / 3) >= 162
-            group by master.throws, 
+            SELECT sum(pitching.ER) AS "earned_runs",
+                sum(pitching.IPouts) / 3 AS "innings_pitched",
+                sum(pitching.H) AS "H_P",
+                sum(pitching.SO) AS "SO_P",
+                sum(pitching.ERA) AS "ERA",
+                sum(pitching.BFP) AS "batters_faced",
+                sum(pitching.BAOpp) AS "Opp_BA",
+                sum(pitching.BB) AS "BB_P",
+                sum(pitching.HR) AS "HR_P",
+                sum(pitching.HBP) AS "HBP_P",
+                sum(pitching.G) AS "G_P",
+                master.throws AS "throwing_arm",
+                master.retroID AS "pit_id",
+                pitching.yearID AS "year"
+            FROM pitching INNER JOIN master ON pitching.playerID = master.playerID
+            WHERE pitching.yearID >= '2012' AND (pitching.IPouts / 3) >= 162
+            GROUP BY master.throws, 
                 master.retroID,
                 pitching.yearID;'''
 
 
 # Query to Pull Batting Data
 batting_query = '''
-            select sum(batting.H) as "H",
-                sum(batting.SO) as "SO",
-                sum(batting.doubles) as "doubles",
-                sum(batting.triples) as "triples",
-                sum(batting.HR) as "HR",
-                sum(batting.AB) as "AB",
-                sum(batting.BB) as "BB",
-                sum(batting.HBP) as "HBP",
-                sum(batting.SF) as "SF",
-                sum(batting.G) as "G",
-                master.bats as "batter_arm",
-                master.retroID as "bat_id",
-                batting.yearID as "year"
-            from batting inner join master on batting.playerID = master.playerID
-            where batting.yearID >= '2012' and batting.AB >= 502
-            group by master.bats,
+            SELECT sum(batting.H) AS "H",
+                sum(batting.SO) AS "SO",
+                sum(batting.doubles) AS "doubles",
+                sum(batting.triples) AS "triples",
+                sum(batting.HR) AS "HR",
+                sum(batting.AB) AS "AB",
+                sum(batting.BB) AS "BB",
+                sum(batting.HBP) AS "HBP",
+                sum(batting.SF) AS "SF",
+                sum(batting.G) AS "G",
+                master.bats AS "batter_arm",
+                master.retroID AS "bat_id",
+                batting.yearID AS "year"
+            FROM batting INNER JOIN master ON batting.playerID = master.playerID
+            WHERE batting.yearID >= '2012' AND batting.AB >= 502
+            GROUP BY master.bats,
                 master.retroID,
                 batting.yearID;'''
 
